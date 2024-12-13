ebb3c244d244e4b5d00ab365257af767eba9a9b0
Gabriel Williams <gabrielskwilliams@gmail.com>
2024-12-12 22:30:53 -0800

    readme update

--- constants/constants.go
+++ constants/constants.go
@@ -1,12 +1,7 @@
 package constants
 
 const (
-	LLM = "gpt-4o"
-	// gpt-3.5-turbo-0613
-	// gpt-4-1106-preview
-	// gpt-4o
-	Embedder = "text-embedding-3-small"
-	// "text-embedding-ada-002"
-
+	LLM                          = "gpt-4o"
+	Embedder                     = "text-embedding-3-small"
 	NonSubscriberFileUploadLimit = 5000000 // 5mb
 )
